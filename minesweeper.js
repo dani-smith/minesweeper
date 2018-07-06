b2403d76fa56b4a410b7edeb1007bbacc9816c2f
unknown <dcis.smith@gmail.com$git config --global user.email dcis.smith@gmail.com>
2018-07-06 12:58:12 +1200

    minesweeper part 1

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,47 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board =  {
+cells: [
+  {row: 0, col: 0, isMine: true, hidden: true,},
+  {row: 0, col: 1, isMine: false, hidden: true,},
+  {row: 0, col: 2, isMine: false, hidden: true,},
+  {row: 0, col: 3, isMine: true, hidden: true,},
+  {row: 0, col: 4, isMine: true, hidden: true,},
+  {row: 0, col: 5, isMine: false, hidden: true,},
+  {row: 1, col: 0, isMine: true, hidden: true,},
+  {row: 1, col: 1, isMine: false, hidden: true,},
+  {row: 1, col: 2, isMine: true, hidden: true,},
+  {row: 1, col: 3, isMine: true, hidden: true,},
+  {row: 1, col: 4, isMine: false, hidden: true,},
+  {row: 1, col: 5, isMine: false, hidden: true,},
+  {row: 2, col: 0, isMine: false, hidden: true,},
+  {row: 2, col: 1, isMine: false, hidden: true,},
+  {row: 2, col: 2, isMine: false, hidden: true,},
+  {row: 2, col: 3, isMine: true, hidden: true,},
+  {row: 2, col: 4, isMine: false, hidden: true,},
+  {row: 2, col: 5, isMine: true, hidden: true,},
+  {row: 3, col: 0, isMine: true, hidden: true,},
+  {row: 3, col: 1, isMine: false, hidden: true,},
+  {row: 3, col: 2, isMine: true, hidden: true,},
+  {row: 3, col: 3, isMine: false, hidden: true,},
+  {row: 3, col: 4, isMine: true, hidden: true,},
+  {row: 3, col: 5, isMine: true, hidden: true,},
+  {row: 4, col: 0, isMine: true, hidden: true,},
+  {row: 4, col: 1, isMine: false, hidden: true,},
+  {row: 4, col: 2, isMine: false, hidden: true,},
+  {row: 4, col: 3, isMine: true, hidden: true,},
+  {row: 4, col: 4, isMine: false, hidden: true,},
+  {row: 4, col: 5, isMine: true, hidden: true,},
+  {row: 5, col: 0, isMine: true, hidden: true,},
+  {row: 5, col: 1, isMine: false, hidden: true,},
+  {row: 5, col: 2, isMine: false, hidden: true,},
+  {row: 5, col: 3, isMine: true, hidden: true,},
+  {row: 5, col: 4, isMine: true, hidden: true,},
+  {row: 5, col: 5, isMine: false, hidden: true,},
+]
+};
+
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
